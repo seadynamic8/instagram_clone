198b6710f62906ba44e4bea28db248d7ec03f188
seadynamic8 <seadynamic8@zoho.com>
2023-08-17 17:21:30 -0400

    initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many_attached :images
+  has_many :likes
+  has_many :likers, through: :likes, source: :user
+  has_many :comments
+
+  validates :images, presence: true, blob: { content_type: :image }
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :posts
+  devise_for :users
+  # get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "home#index"
+
+  post "toggle_like", to: "likes#toggle_like", as: :toggle_like
+
+  resources :comments, only: [:create, :destroy]
+end
