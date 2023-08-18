df4edddf7785c0b9177967dcd2091bae03f8987e
seadynamic8 <seadynamic8@zoho.com>
2023-08-17 20:26:16 -0400

    Make sure likes and comments are destroyed if posts are deleted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :likers, through: :likes, source: :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :images, presence: true, blob: { content_type: :image }
 end
