18f3b40f39a380c04e0fc6800918d34e52a5be44
seadynamic8 <seadynamic8@zoho.com>
2023-08-18 19:40:32 -0400

    Added follows controller and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   has_many :follow_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "followed_id"
 
   has_many :accepted_sent_requests, -> { where(accepted: true) }, class_name: "Follow", foreign_key: "follower_id"
-  # has_many :waiting_sent_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "follower_id"
+  has_many :waiting_sent_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "follower_id"
 
   has_many :followers, through: :accepted_received_requests, source: :follower
   has_many :following, through: :accepted_sent_requests, source: :followed
@@ -24,5 +24,9 @@ class User < ApplicationRecord
 
   def unfollow(user)
     self.accepted_sent_requests.find_by(followed: user)&.destroy
+  end
+
+  def cancel_request(user)
+    self.waiting_sent_requests.find_by(followed: user)&.destroy
   end
 end
