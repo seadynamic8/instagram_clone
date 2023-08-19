fda30fe5fd933b85f72b2ea259827a821dae2508
seadynamic8 <seadynamic8@zoho.com>
2023-08-18 20:30:01 -0400

    Added the view and some model work to complete the follow system

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,8 @@ class User < ApplicationRecord
   has_many :waiting_sent_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "follower_id"
 
   has_many :followers, through: :accepted_received_requests, source: :follower
-  has_many :following, through: :accepted_sent_requests, source: :followed
+  has_many :followings, through: :accepted_sent_requests, source: :followed
+  has_many :waiting_followings, through: :waiting_sent_requests, source: :followed
 
   def follow(user)
     Follow.create(follower: self, followed: user)
