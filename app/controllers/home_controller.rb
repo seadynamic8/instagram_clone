e48d2db51a07cf82bf9bdbf383061317549c9a9c
seadynamic8 <seadynamic8@zoho.com>
2023-08-18 23:51:47 -0400

    Made the suggestions feature work properly with follow buttons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ApplicationController
 
+  before_action :set_suggestions
+
   def index
+  end
+
+  private
+
+  def set_suggestions
+    @suggestions = [current_user.followers]
+    [current_user.followers, current_user.followings].flatten.uniq.each do |f|
+      @suggestions.concat(f.followers, f.followings)
+    end
+    @suggestions = [@suggestions, User.all.sample(10)].flatten.uniq - [current_user.followings, current_user].flatten
+    @suggestions = @suggestions.sample(5)
   end
 end
