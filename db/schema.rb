a292e8b9cc240c99e19791237c823b82a2897a1c
seadynamic8 <seadynamic8@zoho.com>
2023-08-20 11:49:50 -0400

    Change database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_18_144019) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,8 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_18_144019) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -61,8 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_18_144019) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
@@ -74,7 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_18_144019) do
     t.string "caption"
     t.float "longitude"
     t.float "latitute"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.boolean "allow_comments"
     t.boolean "show_likes_count"
     t.datetime "created_at", null: false
